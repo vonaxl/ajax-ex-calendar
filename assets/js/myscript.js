7bf4ed5b62a4adf08e76f232d6bf682ec3013ee9
vonaxl <vonaxlcorso@gmail.com>
2019-11-21 17:44:35 +0100

    added some comments and some minor changes

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,27 +1,45 @@
 function generateDays() {
   // salviamo in una variabile i tot giorni di un mese che abbiamo indicato nel moment seguendo la regola imposta 'DD/MM/YYYY'
-  var numeriGG = moment("01/01/2018", "DD/MM/YYYY").add(m,'months').daysInMonth();
-  
-  var meseOutput =  moment("01/01/2018", "DD/MM/YYYY").add(m,'months').format("MMMM");
+  var numeriGG = moment("01/01/2018", "DD/MM/YYYY")
+    .add(m, "months")
+    .daysInMonth();
+
+  var meseOutput = moment("01/01/2018", "DD/MM/YYYY")
+    .add(m, "months")
+    .format("MMMM").toUpperCase();
   console.log(meseOutput);
   $("#mese").html(meseOutput);
   console.log(numeriGG);
   //Eseguo un ciclo per creare ciascun giorno del mese nel mio html
   for (let i = 1; i <= numeriGG; i++) {
-    var currentDate = moment([2018, m, i]).format("YYYY-MM-DD");
+    var currentDate = moment([y, m, i]).format("YYYY-MM-DD");
     // console.log("TCL: generateDays -> currentDate", currentDate)
     var currentDay = moment(currentDate).format("DD dddd");
     $(".calendario ul").append(
       '<li data-date="' + currentDate + '">' + currentDay + "</li>"
     );
   }
+//   after generating the calendar check if the starting date is sunday if not then function starts (28)
+  sundayCheck();
 }
+
+
+// check if this 8th child is sunday if not then add a blank li
+function sundayCheck(){
+    var sunday = $('ul li:nth-child(8)').html().indexOf('domenica') > -1;
+    console.log(sunday);
+    if (sunday===false) {
+        $(".calendario ul").prepend('<li></li>')
+        sundayCheck();
+    }
+    
+}
+// link month changes if button is clicked, it resets the ul setup fisrt then generate a new calendar with festives
 function dateSetter() {
-    link ="https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=" + m;
-    $(".calendario ul").html("");
-    generateDays();
-    askFestivita();
-    mese = m+1;
+  link ="https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=" + m;
+  $(".calendario ul").html("");
+  generateDays();
+  askFestivita();
 }
 
 function askFestivita() {
@@ -29,12 +47,12 @@ function askFestivita() {
     url: link,
     method: "GET",
     success: function(dati) {
-      console.log(dati);
+    //   console.log(dati);
       // per ogni elemento dentro l'array cerca nel html l'attributo che corrisponde alla z.data e poi cambia il suo colore e aggiungi la festivita
       dati.response.forEach(z => {
         console.log(z.name, z.date);
         $('.calendario ul [data-date="' + z.date + '"]')
-          .css("color", "red")
+          .addClass("festa")
           .append(" - " + z.name);
       });
     },
@@ -45,10 +63,13 @@ function askFestivita() {
 } //function askFestivita ends
 
 $(document).ready(function() {
+
+// basic rules to be change whenever you want, the very backbown of generateDays functions
   m = 0;
   y = 2018;
   dateSetter();
 
+// if next btn is clicked then m++ if it goes higher than max value then goes to 0
   $("#next").click(function() {
     // if (m<13 && m>0) {
     if (m >= 0 && m < 11) {
@@ -57,25 +78,29 @@ $(document).ready(function() {
       m = 0;
     }
     console.log(m);
-    dateSetter(dateSetter);
-    // }
+    dateSetter();
   });
+  
+  // if prev btn is clicked then m-- if it goes lower than max value then goes to 11
   $("#prev").click(function() {
     if (m > 0 && m <= 11) {
       m--;
     } else {
       m = 11;
     }
-
     console.log(m);
-    dateSetter(dateSetter);
+    dateSetter();
   });
-  $('select').on('change', function () {
+
+//   some bonus experiments to give user more choices, each options on select tag has value then 'm' takes its value
+  $("select").on("change", function() {
     // salva l'elemento cambiato
     var xData = $("option:selected", this);
-    var month=xData.val();
-    m=month;
-    dateSetter(dateSetter);
-    });
+    var month = xData.val();
+    // save its value then change it
+    m = month;
+    dateSetter();
+  });
 
+ 
 });
